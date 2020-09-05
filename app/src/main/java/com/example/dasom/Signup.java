0088ab362b785655fb6ff9b58d14b5d23c62a37c
yechan0704 <key07040@naver.com>
2020-09-06 04:38:32 +0900

    PINSignUp

--- app/src/main/java/com/example/dasom/Signup.java
+++ app/src/main/java/com/example/dasom/Signup.java
@@ -17,38 +17,57 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.andrognito.pinlockview.IndicatorDots;
+import com.andrognito.pinlockview.PinLockListener;
+import com.andrognito.pinlockview.PinLockView;
 import com.example.dasom.api.NetworkHelper;
 import com.example.dasom.data.UserJoin;
+import com.example.dasom.data.UserLogin;
 import com.example.dasom.databinding.ActivityMainBinding;
 import com.example.dasom.databinding.ActivitySignupBinding;
 
 public class Signup extends AppCompatActivity {
 
     private ActivitySignupBinding binding;
-    String phoneNumber, pw, name;
+    String phoneNumber, pw;
+    private final static String TAG = "asdasd";
+    PinLockView mPinLockView;
+    IndicatorDots mIndicatorDots;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signup);
+        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
+        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
 
+
+        binding.setText("PIN번호를 입력해주세요");
 
         //전화번호
-        phoneNumber = "01024169913";
+        phoneNumber = "01024469844";
+
+
+        mPinLockView.attachIndicatorDots(mIndicatorDots);
+        mPinLockView.setPinLockListener(mPinLockListener);
 
 
 
 
 
-        binding.signUpBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pw = binding.pw.getText().toString();
-                if (binding.checkPw.getText().toString() == pw){
+    }
+    private PinLockListener mPinLockListener = new PinLockListener() {
+        @Override
+        public void onComplete(String pin) {
+            Log.e(TAG, "Pin complete: " + pin);
 
+            if (pw!=null){
+                if (pw.equals(pin)){
                     NetworkHelper.getInstance().SignUp(phoneNumber,pw).enqueue(new Callback<UserJoin>() {
                         @Override
                         public void onResponse(Call<UserJoin> call, Response<UserJoin> response) {
+                            Log.e("asdd",response.code()+"");
                             UserJoin userJoin = response.body();
                             Log.e("asd",userJoin.getMessage());
                             Intent intent1 = new Intent(Signup.this,LoginActivity.class);
@@ -58,15 +77,36 @@ public class Signup extends AppCompatActivity {
                         }
                         @Override
                         public void onFailure(Call<UserJoin> call, Throwable t) {
+                            Log.e("asdd","실패");
                         }
                     });
-
                 }else{
-                    Toast.makeText(Signup.this, "pin번호틀림", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Signup.this, "비밀번호 틀림", Toast.LENGTH_SHORT).show();
+                    binding.setText("PIN번호를 다시 입력해주세요");
+                    mPinLockView.resetPinLockView();
                 }
 
 
+
+            }else{
+                pw = pin;
+                binding.setText("PIN번호를 다시 입력해주세요");
+                mPinLockView.resetPinLockView();
             }
-        });
-    }
+
+
+
+
+        }
+
+        @Override
+        public void onEmpty() {
+            Log.e(TAG, "Pin empty");
+        }
+
+        @Override
+        public void onPinChange(int pinLength, String intermediatePin) {
+            Log.e(TAG, "Pin changed, new length " + pinLength + " with intermediate pin " + intermediatePin);
+        }
+    };
 }
