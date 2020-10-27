95b4c64638b79a336053937c6d37f34d7b7f0624
roian6 <roian6@naver.com>
2020-10-28 05:11:56 +0900

    2번째 화면 디자인을 일부 수정했습니다.

--- app/src/main/java/com/example/dasom/screen/main2/Main2Fragment.java
+++ app/src/main/java/com/example/dasom/screen/main2/Main2Fragment.java
@@ -9,21 +9,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import com.example.dasom.R;
 import com.example.dasom.api.NetworkHelper;
 import com.example.dasom.databinding.FragmentMain2Binding;
 import com.example.dasom.screen.login.LoginActivity;
-import com.example.dasom.screen.main1.DiaryBody;
 import com.example.dasom.util.TokenCache;
 import com.example.dasom.util.UserCache;
 import com.google.gson.Gson;
@@ -32,7 +27,11 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-public class Main2Fragment extends Fragment{
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Main2Fragment extends Fragment {
 
     public static Main2Fragment newInstance() {
         return new Main2Fragment();
@@ -46,6 +45,7 @@ public class Main2Fragment extends Fragment{
     private static final String BASE_URL = "https://api.taemin.dev/dasomi/";
     private String token;
     private SettingData body;
+
     @Override
     public void onAttach(@NotNull Context context) {
         super.onAttach(context);
@@ -56,11 +56,11 @@ public class Main2Fragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main2, container, false);
-        SharedPreferences sharedPreferences = mContext.getSharedPreferences("talk_cycle_time",Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences("talk_cycle_time", Context.MODE_PRIVATE);
 
-        token =TokenCache.getToken(mContext);
+        token = TokenCache.getToken(mContext);
 
-        updateInfo(BASE_URL,token);
+        updateInfo(BASE_URL, token);
 
         binding.updateInfoDialogBt.setOnClickListener(v -> {
             UpdateEmergencyDialog updateEmergencyDialog = new UpdateEmergencyDialog(mContext);
@@ -68,17 +68,16 @@ public class Main2Fragment extends Fragment{
 
         });
 
-
-//        TODO: migrate logout from first tab
         binding.btnMainLogout.setOnClickListener(view -> {
             UserCache.clear(mContext);
             TokenCache.clear(mContext);
             startActivity(new Intent(mContext, LoginActivity.class));
 
             Activity a = getActivity();
-            if(a!=null) a.finish();
+            if (a != null) a.finish();
         });
-        binding.talkCycleChangingTv.setText(sharedPreferences.getString("talk_cycle_time","30분 >"));
+
+        binding.talkCycleChangingTv.setText(sharedPreferences.getString("talk_cycle_time", "30분 >"));
 
         binding.talkCycleChangingTv.setOnClickListener(v -> {
 
@@ -91,29 +90,30 @@ public class Main2Fragment extends Fragment{
 
     }
 
-    public void updateInfo(String url, String token){
+    public void updateInfo(String url, String token) {
         NetworkHelper.getInstance(url).updateInfo("Bearer " + token).enqueue(new Callback<SettingData>() {
             @Override
             public void onResponse(Call<SettingData> call, Response<SettingData> response) {
                 if (response.code() != 200) {
                     try {
                         Gson gson = new Gson();
-                        body = gson.fromJson(response.errorBody().string(),SettingData.class);
+                        body = gson.fromJson(response.errorBody().string(), SettingData.class);
                     } catch (IOException ex) {
                         ex.printStackTrace();
                         return;
                     }
                 } else {
                     body = response.body();
-                    try {
-                        binding.setWhenCreate(body.getData().getDataLength()+"");
-                        binding.setUntilNow(body.getData().getLastDataCount()+"");
-
-                    }catch (NullPointerException e){
-                        binding.setWhenCreate("0");
-                        binding.setUntilNow("0");
-
-                    };
+                }
+                Log.d("baam", "onResponse: " + body);
+                Log.d("baam", "onResponse: " + new Gson().toJson(body));
+                try {
+                    binding.setWhenCreate(body.getData().getDataLength() + "");
+                    binding.setUntilNow(body.getData().getLastDataCount() + "");
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                    binding.setWhenCreate("0");
+                    binding.setUntilNow("0");
                 }
 
             }
--- app/src/main/java/com/example/dasom/screen/main2/UpdateInfoValue.java
+++ app/src/main/java/com/example/dasom/screen/main2/UpdateInfoValue.java
@@ -4,8 +4,28 @@ public class UpdateInfoValue {
     private String contact;
     private String username;
 
+    public UpdateInfoValue() {
+
+    }
+
     public UpdateInfoValue(String contact, String username) {
         this.contact = contact;
+        this.username = username;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
     }
 }
